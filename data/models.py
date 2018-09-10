c962ff3e49f7c529564e08c34abdc833c7c7abe1
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2018-09-09 23:55:11 -0500

    added officer and complaint models

--- data/models.py
+++ data/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Officer(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=30)
+    DSN = models.IntegerField()
+    rank = models.CharField(max_length=20)
+
+    def __str__(self):
+        return (self.first_name + ' ' + self.last_name)
+
+class Complaint(models.Model):
+    officer = models.ForeignKey(Officer, on_delete=models.CASCADE)
+    complaint_text = models.CharField(max_length=1000)
+    complaint_date = models.DateTimeField()
