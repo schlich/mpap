e914045e6eee0f8b3629f6ea1043e18c86e5cb87
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2018-09-10 23:38:50 -0500

    added details page

--- data/urls.py
+++ data/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+appname="data"
+
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('', views.index, name="index"),
+    path('<int:officer_id>/', views.detail, name="detail")
 ]
--- data/views.py
+++ data/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Officer
+from .models import Complaint
 
 def index(request):
-    return HttpResponse("Welcome to the Missouri Police Accountability project.")
+    officer_list = Officer.objects.all()
+    complaints_list = Complaint.objects.all()
+    context = {
+        'officer_list': officer_list,
+        'complaints_list': complaints_list,
+    }
+    return render(request,'data/index.html',context)
+
+def detail(request, officer_id):
+    officer = get_object_or_404(Officer, pk=officer_id)
+    return render(request,'data/detail.html',{'officer':officer})
